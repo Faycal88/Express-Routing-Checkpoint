28e69d9fc522f8c3188d288f68905d3d3d441bf3
Faycal88 <99132896+Faycal88@users.noreply.github.com>
2022-04-23 21:58:26 +0100

    Checkpoint ready

--- app.js
+++ app.js
@@ -23,9 +23,13 @@ function getCurrentTimeToString() {
 
 function getDiff(moment) {
     var x = moment().diff(moment().day('7').hour(9).minute(0).second(0), 'milliseconds');
-    
-    console.log(y);
-    return -x;
+    var y = moment().valueOf();
+    return -x+y;
+}
+
+function getClosingHour(moment) {  // i will add the closing hour to the moment later i could not find a way to do it for every day.
+    var hour = moment().hour(17).minute(0).second(0);
+    return hour;
 }
 
 
@@ -36,11 +40,12 @@ app.get('/', (req, res, next) => {
 }, (req, res) => {
     if (req.day == 1 || req.day == 2 || req.day == 3 || req.day == 4 || req.day == 5) {
         res.render('index', { title: "Home" })
-        console.log(req.day);
-    } else
-        getDiff(moment);
-        app.set('layout', './timer.ejs')
-        res.render('timer', {title: "timer", time: req.time })
+    } else 
+        if (req.day == 6 || req.day == 7) {
+            app.set('layout', './timer.ejs')
+            res.render('timer', {title: getDiff(moment) })
+        }
+        
 }
 )
 
